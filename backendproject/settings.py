8f683620f52e0923a9b8fc18c9f8e1cef8ac2b5b
manikolar55 <mani.kolar55@gmail.com>
2022-08-13 20:35:00 +0500

    set Debug False

--- backendproject/settings.py
+++ backendproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-903$(v(jo7ubkkevw)mau+csm^c2*q-$iy!@@@n2matzzbmzgi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
