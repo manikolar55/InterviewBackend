dae9f0b687f07384479cea23be239581cbc6f51f
manikolar55 <mani.kolar55@gmail.com>
2022-08-13 20:11:05 +0500

    remove_static

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
-# from .views import Backend
-from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 from .views import ChartData
 
 urlpatterns = [
